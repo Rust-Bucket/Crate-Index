18e76b75c08f0fdc5d812b89c90ffd8033691c00
danieleades <33452915+danieleades@users.noreply.github.com>
2020-02-09 13:07:28 +0000

    properly implement Config struct
    * add setter+getter methods for Config struct
    * add unit tests
    * cargo fmt

--- src/config.rs
+++ src/config.rs
@@ -3,8 +3,9 @@ use std::fmt;
 use url::Url;
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
 pub struct Config {
-    dl: Url,
+    dl: String,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     api: Option<Url>,
@@ -14,12 +15,38 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(crate_download: Url) -> Self {
+    pub fn new(crate_download: impl Into<String>) -> Self {
+        let crate_download = crate_download.into();
+
+        debug_assert!(Url::parse(&crate_download).is_ok());
+
         Self {
             dl: crate_download,
             api: None,
             allowed_registries: Vec::default(),
         }
+    }
+
+    pub fn with_api(mut self, api: Url) -> Self {
+        self.api = Some(api);
+        self
+    }
+
+    pub fn with_allowed_registry(mut self, registry: Url) -> Self {
+        self.allowed_registries.push(registry);
+        self
+    }
+
+    pub fn download(&self) -> &String {
+        &self.dl
+    }
+
+    pub fn api(&self) -> &Option<Url> {
+        &self.api
+    }
+
+    pub fn allowed_registries(&self) -> impl Iterator<Item = &Url> {
+        self.allowed_registries.iter()
     }
 }
 
@@ -36,9 +63,73 @@ mod tests {
 
     #[test]
     fn new() {
-        let url = Url::parse("https://crates.io/api/v1/crates/{crate}/{version}/download")
-            .expect("URL is invalid!");
+        let url = "https://crates.io/api/v1/crates/{crate}/{version}/download";
 
         let _ = Config::new(url);
+    }
+
+    #[test]
+    fn set_and_get() {
+        let url = "https://my-crates-server.com/api/v1/crates/{crate}/{version}/download";
+        let api = Url::parse("https://my-crates-server.com/").unwrap();
+        let registries = vec![
+            Url::parse("https://github.com/rust-lang/crates.io-index").unwrap(),
+            Url::parse("https://my-intranet:8080/index").unwrap(),
+        ];
+
+        let config = Config::new(url)
+            .with_api(api.clone())
+            .with_allowed_registry(registries[0].clone())
+            .with_allowed_registry(registries[1].clone());
+
+        assert_eq!(config.download(), &url);
+        assert_eq!(config.api(), &Some(api));
+        assert!(config.allowed_registries().eq(registries.iter()));
+    }
+
+    #[test]
+    fn format_simple() {
+        let url = "https://crates.io/api/v1/crates/{crate}/{version}/download";
+
+        let config = Config::new(url);
+
+        let expected = r#"{
+  "dl": "https://crates.io/api/v1/crates/{crate}/{version}/download"
+}"#
+        .to_string();
+
+        let actual = config.to_string();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn format_full() {
+        let url = "https://my-crates-server.com/api/v1/crates/{crate}/{version}/download";
+        let api = Url::parse("https://my-crates-server.com/").unwrap();
+
+        let config = Config::new(url)
+            .with_api(api)
+            .with_allowed_registry(
+                Url::parse("https://github.com/rust-lang/crates.io-index").unwrap(),
+            )
+            .with_allowed_registry(Url::parse("https://my-intranet:8080/index").unwrap());
+
+        let expected: serde_json::Value = serde_json::from_str(
+            r#"
+            {
+                "dl": "https://my-crates-server.com/api/v1/crates/{crate}/{version}/download",
+                "api": "https://my-crates-server.com/",
+                "allowed-registries": [
+                    "https://github.com/rust-lang/crates.io-index",
+                    "https://my-intranet:8080/index"
+                ]
+            }"#,
+        )
+        .unwrap();
+
+        let actual: serde_json::Value = serde_json::from_str(&config.to_string()).unwrap();
+
+        assert_eq!(expected, actual);
     }
 }
