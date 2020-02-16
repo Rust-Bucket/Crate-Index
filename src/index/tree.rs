2f30adbcc54e627a4e110d9634ad540e94764973
Daniel Eades <danieleades@hotmail.com>
2020-02-16 09:50:14 +0000

    add additional documentation comments and getters

--- src/index/tree.rs
+++ src/index/tree.rs
@@ -1,7 +1,7 @@
 use super::Config;
 use crate::{
     index::{IndexFile, Metadata},
-    validate, Error, Result,
+    Result,
 };
 use async_std::path::PathBuf;
 use std::io;
@@ -13,33 +13,89 @@ pub struct Tree {
     config: Config,
 }
 
+/// Builder for creating a new [`Tree`]
 pub struct TreeBuilder {
     root: PathBuf,
     config: Config,
 }
 
 impl TreeBuilder {
+    /// Set the Url for the registry API.
+    ///
+    /// The API should implement the REST interface as defined in [the Cargo book](https://doc.rust-lang.org/cargo/reference/registries.html)
     pub fn api(mut self, api: Url) -> Self {
         self.config = self.config.with_api(api);
         self
     }
 
+    /// Add an allowed registry.
+    ///
+    /// Crates in this registry are only allowed to have dependencies which are
+    /// also in this registry, or in one of the allowed registries.
+    ///
+    /// Add multiple registries my calling this method multiple times.
     pub fn allowed_registry(mut self, registry: Url) -> Self {
         self.config = self.config.with_allowed_registry(registry);
         self
     }
 
+    /// Add crates.io as an allowed registry.
+    ///
+    /// You will almost always want this, so this exists as a handy shortcut.
     pub fn allow_crates_io(mut self) -> Self {
         self.config = self.config.with_crates_io_registry();
         self
     }
 
     pub async fn build(self) -> io::Result<Tree> {
+        // once 'IntoFuture' is stabilised, this 'build' method should be replaced with
+        // an 'IntoFuture' implementation so that the builder can be awaited directly
         Tree::new(self.root, self.config).await
     }
 }
 
 impl Tree {
+    /// Create a new index `Tree`.
+    ///
+    /// The root path, and the URL for downloading .crate files is required.
+    /// Additional options can be set using the builder API (see
+    /// [`TreeBuilder`] for options).
+    ///
+    /// # Example
+    ///
+    /// ## Basic Config
+    ///
+    /// ```no_run
+    /// use crate_index::index::Tree;
+    /// # use crate_index::Error;
+    /// # async {
+    /// let root = "/index";
+    /// let download = "https://my-crates-server.com/api/v1/crates/{crate}/{version}/download";
+    ///
+    /// let index_tree = Tree::init(root, download).build().await?;
+    /// # Ok::<(), Error>(())
+    /// # };
+    /// ```
+    ///
+    /// ## More Options
+    ///
+    /// ```no_run
+    /// use crate_index::{index::Tree, Url};
+    /// # use crate_index::Error;
+    /// # async {
+    /// let root = "/index";
+    /// let download = "https://my-crates-server.com/api/v1/crates/{crate}/{version}/download";
+    ///
+    ///
+    /// let index_tree = Tree::init(root, download)
+    ///     .api(Url::parse("https://my-crates-server.com/").unwrap())
+    ///     .allowed_registry(Url::parse("https://my-intranet:8080/index").unwrap())
+    ///     .allow_crates_io()
+    ///     .build()
+    ///     .await?;
+    /// # Ok::<(), Error>(())
+    /// # };
+    /// ```
     pub fn init(root: impl Into<PathBuf>, download: impl Into<String>) -> TreeBuilder {
         let root = root.into();
         let config = Config::new(download);
@@ -54,6 +110,12 @@ impl Tree {
         Ok(tree)
     }
 
+    /// Open an existing index tree at the given root path.
+    ///
+    /// # Errors
+    ///
+    /// This method can fail if the given path does not exist, or the config
+    /// file cannot be read.
     pub async fn open(root: impl Into<PathBuf>) -> io::Result<Self> {
         let root = root.into();
         let config = Config::from_file(&root).await?;
@@ -80,5 +142,21 @@ impl Tree {
     /// The location on the filesystem of the root of the index
     pub fn root(&self) -> &PathBuf {
         &self.root
+    }
+
+    /// The Url for downloading .crate files
+    pub fn download(&self) -> &String {
+        self.config.download()
+    }
+
+    /// The Url of the API
+    pub fn api(&self) -> &Option<Url> {
+        self.config.api()
+    }
+
+    /// The list of registries which crates in this index are allowed to have
+    /// dependencies on
+    pub fn allowed_registries(&self) -> &Vec<Url> {
+        self.config.allowed_registries()
     }
 }
